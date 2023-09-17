e44c6abd00dbff82e4ef90fc648e2f7964797b72
giancarlo-pereira <giancarlo.pereira@columbia.edu>
2023-09-17 18:35:50 -0400

    merged Akshay's changes into our branch

--- clock_game.sh
+++ clock_game.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
 if [[ $1 == "True" ]] ; then
-<<<<<<< HEAD
-	python clock_game.py -ng $1 -s $2
-else
-	python clock_gui.py -s $2 &
-	sleep 1
-	python clock_game.py -s $2 &
-=======
     python clock_game.py -ng $1 -s $2
 else
     python clock_gui.py -s $2 &
     sleep 1
     python clock_game.py -s $2 &
->>>>>>> 57424c6858998b97c5dfa2aa2ce1cb92284f1e1b
 fi
